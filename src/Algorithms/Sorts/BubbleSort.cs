3f73436b8bcdc5045c83675dd7171b28fdb12c43
mrunowen <birenchens@outlook.com>
2020-05-06 22:13:14 +0800

    add: bubble sort

---
+++ src/Algorithms/Sorts/BubbleSort.cs
@@ -0,0 +1,40 @@
+﻿/*
+ *  冒泡排序
+ *  
+ *  算法：
+ *      1、从第一个数开始遍历，每个数都逐个比较
+ *      
+ *  说明：
+ *      冒泡排序唯一的用处就是用来对比其他排序是多么优秀，所以是算法书的常客
+ */
+
+namespace Algorithms.Sorts
+{
+    /// <summary>
+    /// 冒泡排序
+    /// </summary>
+    public class BubbleSort
+    {
+        /// <summary>
+        /// 冒泡排序
+        /// </summary>
+        /// <param name="value">待排序的数组</param>
+        /// <returns>排序后的数组</returns>
+        public int[] Sort(int[] value)
+        {
+            for (int i = 0; i < value.Length - 1; i++)
+            {
+                for (int j = 0; j < value.Length - 1 - i; j++)
+                {
+                    if (value[j] > value[j + 1])
+                    {
+                        int temp = value[j];
+                        value[j] = value[j + 1];
+                        value[j + 1] = temp;
+                    }
+                }
+            }
+            return value;
+        }
+    }
+}
--- src/AlgorithmsTest/SortTest.cs
+++ src/AlgorithmsTest/SortTest.cs
@@ -10,6 +10,25 @@ namespace AlgorithmsTest
     public class SortTest
     {
         /// <summary>
+        /// ≤‚ ‘√∞≈›≈≈–Ú
+        /// </summary>
+        [TestMethod]
+        public void TestBubbleSort()
+        {
+            int[] value = { 2, 332, 19, 4, 091, -32 };
+            int[] afterSort = { -32, 2, 4, 19, 091, 332 };
+
+            BubbleSort bubbleSort = new BubbleSort();
+            var sortedValue = bubbleSort.Sort(value);
+
+            for (int i = 0; i < sortedValue.Length; i++)
+            {
+                if (sortedValue[i] != afterSort[i])
+                    Assert.Fail("≈≈–Ú ß∞‹");
+            }
+        }
+
+        /// <summary>
         /// ≤‚ ‘øÏÀŸ≈≈–Ú
         /// </summary>
         [TestMethod]
