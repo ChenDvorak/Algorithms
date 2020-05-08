13b812e3d2f4fb7e98dfe8ebe701525e5f47d8f9
myfor <mrmyfor@outlook.com>
2020-05-08 11:52:59 +0800

    update: labyrinth

--- src/Algorithms/DeepFirstSearch/Labyrinth.cs
+++ src/Algorithms/DeepFirstSearch/Labyrinth.cs
@@ -1,11 +1,12 @@
 ﻿/*
  * 使用深度优先搜索探索迷宫
- * 
+ * 由于是使用穷举，每种分支都会走一遍，会导致搜索时间特别长
+ * 所以想这种迷宫的地图搜索，不要使用，效率是十分地下的。
+ * 像 9 * 8 的地图，都会花费 12 秒的时间。
  */
 
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Algorithms.DeepFirstSearch
 {
@@ -21,78 +22,66 @@ namespace Algorithms.DeepFirstSearch
         {
             public int X { get; set; }
             public int Y { get; set; }
+            public Coordinate(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
 
             public override string ToString()
             {
-                return $"{X},{Y}";
+                return $"{{{X},{Y}}}";
+            }
+
+            public override int GetHashCode()
+            {
+                string value = $"{X}0{Y}";
+                int code = int.Parse(value);
+                return code;
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (!(obj is Coordinate))
+                    return false;
+                Coordinate compareValue = (Coordinate)obj;
+                return GetHashCode() == compareValue.GetHashCode();
             }
 
             /// <summary>
             /// 往左走，获取下一个坐标
             /// </summary>
             /// <returns>新坐标</returns>
-            internal Coordinate MoveLeft()
-            {
-                return new Coordinate
-                {
-                    X = X - 1,
-                    Y = Y
-                };
-            }
+            internal Coordinate MoveLeft() => new Coordinate(X - 1, Y);
             /// <summary>
             /// 往右走，获取下一个坐标
             /// </summary>
             /// <returns>新坐标</returns>
-            internal Coordinate MoveRight()
-            {
-                return new Coordinate
-                {
-                    X = X + 1,
-                    Y = Y
-                };
-            }
+            internal Coordinate MoveRight() => new Coordinate(X + 1, Y);
             /// <summary>
             /// 往上走，获取下一个坐标
             /// </summary>
             /// <returns>新坐标</returns>
-            internal Coordinate MoveUp()
-            {
-                return new Coordinate
-                {
-                    X = X,
-                    Y = Y - 1
-                };
-            }
+            internal Coordinate MoveUp() => new Coordinate(X, Y - 1);
             /// <summary>
             /// 往下走，获取下一个坐标
             /// </summary>
             /// <returns>新坐标</returns>
-            internal Coordinate MoveDown()
-            {
-                return new Coordinate
-                {
-                    X = X,
-                    Y = Y + 1
-                };
-            }
+            internal Coordinate MoveDown() => new Coordinate(X, Y + 1);
         }
 
         /// <summary>
         /// 地图
         /// </summary>
-        private readonly char[][] map = {};
+        private readonly char[][] map = { };
         /// <summary>
         /// 可通行的道路
         /// </summary>
-        private const char ROAD = '.';
+        public const char ROAD = '.';
         /// <summary>
         /// 障碍物
         /// </summary>
-        private const char BARRIER = '#';
-        /// <summary>
-        /// 目标
-        /// </summary>
-        private const char TARGET = '0';
+        public const char BARRIER = '#';
         /// <summary>
         /// 开始坐标
         /// </summary>
@@ -102,15 +91,29 @@ namespace Algorithms.DeepFirstSearch
         /// </summary>
         public readonly Coordinate Target_Coordinate;
         /// <summary>
+        /// 行数
+        /// </summary>
+        private readonly int Rows;
+        /// <summary>
+        /// 列数
+        /// </summary>
+        private readonly int Columns;
+        /// <summary>
         /// 经过的坐标标记
         /// </summary>
-        private HashSet<string> Book { get; set; }
+        private HashSet<Coordinate> Book { get; set; }
         /// <summary>
-        /// 路径
+        /// 最短路径
         /// </summary>
-        private List<Coordinate> Path { get; set; }                         
-
-        public bool IsPass(Coordinate coordinate) => Book.Contains(coordinate.ToString());
+        private Coordinate[] Path { get; set; }
+        /// <summary>
+        /// 临时路径
+        /// </summary>
+        private Stack<Coordinate> TemplatePath { get; set; }
+        /// <summary>
+        /// 最小步数
+        /// </summary>
+        private int MinimumStep;
 
         private Labyrinth(char[][] map, Coordinate startCoordinate, Coordinate targetCoordinate)
         {
@@ -121,9 +124,14 @@ namespace Algorithms.DeepFirstSearch
             Start_Coordinate = startCoordinate;
             Target_Coordinate = targetCoordinate;
 
-            int capacity = map.Length * map[0].Length / 2;
-            Book = new HashSet<string>(capacity);
-            Path = new List<Coordinate>(capacity);
+            Rows = map.Length;
+            Columns = map[0].Length;
+            MinimumStep = Rows * Columns;
+
+            int capacity = MinimumStep / 2;
+            Book = new HashSet<Coordinate>(capacity);
+
+            TemplatePath = new Stack<Coordinate>(capacity);
         }
         /// <summary>
         /// 新建一个迷宫
@@ -132,7 +140,7 @@ namespace Algorithms.DeepFirstSearch
         /// <param name="startCoordinate">开始坐标</param>
         /// <param name="targetCoordinate">目标坐标</param>
         /// <returns></returns>
-        public Labyrinth New(char[][] map, Coordinate startCoordinate, Coordinate targetCoordinate)
+        public static Labyrinth New(char[][] map, Coordinate startCoordinate, Coordinate targetCoordinate)
         {
             return new Labyrinth(map, startCoordinate, targetCoordinate);
         }
@@ -143,9 +151,62 @@ namespace Algorithms.DeepFirstSearch
         /// <returns>(是否可以达到目的, 达到目的最小路径)</returns>
         public (bool, Coordinate[]) Search()
         {
+            SearchMinimumPath(Start_Coordinate, 0);
+            if (MinimumStep < 0)
+                return (false, Array.Empty<Coordinate>());
+            return (true, Path);
+        }
 
+        /// <summary>
+        /// 搜索最小路径
+        /// </summary>
+        /// <param name="currentCoordinate">当前坐标</param>
+        /// <param name="step">已经走了几步</param>
+        /// <returns>是否有找到最小路径</returns>
+        private bool SearchMinimumPath(Coordinate currentCoordinate, int step)
+        {
+            if (currentCoordinate.Equals(Target_Coordinate))
+            {
+                //  更新最小路径记录
+                if (step < MinimumStep)
+                {
+                    MinimumStep = step;
+                    //  更新最小路径记录
+                    Path = new Coordinate[TemplatePath.Count];
+                    TemplatePath.CopyTo(Path, 0);
+                    return true;
+                }
+                return false;
+            }
 
-            throw new NotImplementedException();
+            bool isFindMinimumPath = false;
+
+            //  往四个方向探索
+            for (int i = 0; i < 4; i++)
+            {
+                Coordinate nextCoordinate = i switch
+                {
+                    0 => currentCoordinate.MoveUp(),
+                    1 => currentCoordinate.MoveRight(),
+                    2 => currentCoordinate.MoveDown(),
+                    3 => currentCoordinate.MoveLeft(),
+                    _ => throw new ArgumentOutOfRangeException(),
+                };
+
+                if (Book.Contains(nextCoordinate) || map[nextCoordinate.X][nextCoordinate.Y] == BARRIER
+                || nextCoordinate.X < 0 || nextCoordinate.X > Rows
+                || nextCoordinate.Y < 0 || nextCoordinate.Y > Columns)
+                    continue;
+
+                TemplatePath.Push(nextCoordinate);
+                Book.Add(nextCoordinate);
+                isFindMinimumPath = SearchMinimumPath(nextCoordinate, step + 1);
+                //  记录找到的最小路径
+
+                TemplatePath.Pop();
+                Book.Remove(nextCoordinate);
+            }
+            return isFindMinimumPath;
         }
     }
 }
---
+++ src/AlgorithmsTest/DFSTest.cs
@@ -0,0 +1,46 @@
+﻿using Algorithms.DeepFirstSearch;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace AlgorithmsTest
+{
+    /// <summary>
+    /// 测试深度优先搜索
+    /// </summary>
+    [TestClass]
+    public class DFSTest
+    {
+        /// <summary>
+        /// 测试走迷宫
+        /// </summary>
+        [TestMethod]
+        public void TestLabyrinth()
+        {
+            char[][] map = new char[][] {
+                new char[] {'#', '#', '#', '#', '#', '#', '#', '#' },
+                new char[] {'#', '.', '.', '.', '.', '.', '.', '#' },
+                new char[] {'#', '.', '.', '.', '.', '.', '.', '#' },
+                new char[] {'#', '.', '#', '.', '.', '.', '.', '#' },
+                new char[] {'#', '.', '.', '.', '.', '#', '.', '#' },
+                new char[] {'#', '.', '.', '.', '.', '.', '.', '#' },
+                new char[] {'#', '.', '.', '#', '.', '#', '.', '#' },
+                new char[] {'#', '.', '.', '.', '.', '#', '.', '#' },
+                new char[] {'#', '.', '.', '.', '.', '.', '.', '#' },
+                new char[] {'#', '#', '#', '#', '#', '#', '#', '#' },
+            };
+
+            var startCooridnate = new Labyrinth.Coordinate(1, 1);
+            var endCooridnate = new Labyrinth.Coordinate(7, 6);
+
+            var labyrinth = Labyrinth.New(map, startCooridnate, endCooridnate);
+            //  即使只是 8 * 9 的地图，搜索的效率也十分低下
+            (bool isFind, Labyrinth.Coordinate[] path) = labyrinth.Search();
+            Assert.IsTrue(isFind);
+            //  输出路径
+            for (int i = path.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine($"{path[i].ToString()} ");
+            }
+        }
+    }
+}
