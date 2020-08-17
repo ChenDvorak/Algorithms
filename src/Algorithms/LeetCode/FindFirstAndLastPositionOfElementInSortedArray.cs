acc0a7a9adf188db0eb9e92af012fcc64fdbd057
myfor <mrmyfor@outlook.com>
2020-08-17 17:46:21 +0800

    add algorithm of find first and last position of element in sorted array that not completed

---
+++ src/Algorithms/LeetCode/FindFirstAndLastPositionOfElementInSortedArray.cs
@@ -0,0 +1,22 @@
+﻿/*
+ *  find first and last position of element in sorted array
+ *  given an array of integers nums sorted in ascending order
+ *  find the starting and ending position of a given target value
+ *  your algorithm's runtime complexity must be in order of O(log n)
+ *  if the target is not found an array, return [-1, -1]
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.LeetCode
+{
+    public class FindFirstAndLastPositionOfElementInSortedArray
+    {
+        public int[] Run(int[] nums, int target)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
