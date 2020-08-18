d265d7b1de8de3bb7d7b82a0476df57a097a32f6
myfor <mrmyfor@outlook.com>
2020-08-18 17:08:37 +0800

    add find first and last positions of element in sorted array

--- src/Algorithms/LeetCode/FindFirstAndLastPositionOfElementInSortedArray.cs
+++ src/Algorithms/LeetCode/FindFirstAndLastPositionOfElementInSortedArray.cs
@@ -6,17 +6,31 @@
  *  if the target is not found an array, return [-1, -1]
  */
 
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace Algorithms.LeetCode
 {
     public class FindFirstAndLastPositionOfElementInSortedArray
     {
         public int[] Run(int[] nums, int target)
         {
-            throw new NotImplementedException();
+            int firstIndex = FindIndex(nums, target, true);
+            if (nums[firstIndex] != target) return new int[] { -1, -1 };
+            int lastIndex = FindIndex(nums, target, false);
+            return new int[] { firstIndex, lastIndex };
+        }
+
+        private int FindIndex(int[] nums, int target, bool findLeft)
+        {
+            int left = 0, right = nums.Length - 1;
+
+            while (left < right)
+            {
+                int mid = (left + right) >> 1;
+                if (nums[mid] > target || (findLeft && nums[mid] == target))
+                    right = mid;
+                else
+                    left = mid + 1;
+            }
+            return left;
         }
     }
 }
