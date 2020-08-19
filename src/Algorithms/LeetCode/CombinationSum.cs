9280c03d0294edb09d7c8736dc0b1050ccb3b8b3
myfor <mrmyfor@outlook.com>
2020-08-19 14:28:59 +0800

    add combination sum

---
+++ src/Algorithms/LeetCode/CombinationSum.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algorithms.LeetCode
+{
+    public class CombinationSum
+    {
+
+        public List<List<int>> Run(int[] nums, int target)
+        {
+            Array.Sort(nums);
+            if (nums.Contains(0))
+                throw new ArgumentException("not allow zero");
+
+            List<List<int>> answers = new List<List<int>>(nums.Length);
+            Dfs(nums, target, 0, answers, new Stack<int>(nums.Length));
+            return answers;
+        }
+
+        private void Dfs(int[] nums, int residue, int begin, List<List<int>> answers, Stack<int> path)
+        {
+            if (residue == 0)
+            {
+                answers.Add(path.ToList());
+                return;
+            }
+
+            for (int i = begin; i < nums.Length; i++)
+            {
+                if (nums[i] > residue)
+                    break;
+
+                path.Push(nums[i]);
+                Dfs(nums, residue - nums[i], i, answers, path);
+                path.Pop();
+            }
+        }
+    }
+}
---
+++ src/AlgorithmsTest/LeetCode/CombinationSumTest.cs
@@ -0,0 +1,24 @@
+﻿using Algorithms.LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmsTest.LeetCode
+{
+    /// <summary>
+    /// 测试深度优先搜索
+    /// </summary>
+    [TestClass]
+    public class CombinationSumTest
+    {
+        /// <summary>
+        /// 测试使用深度优先搜索走迷宫
+        /// </summary>
+        [TestMethod]
+        public void TestCombinationSum()
+        {
+            var f = new CombinationSum();
+
+            var answers = f.Run(new int[] { 2, 3, 6, 7 }, 7);
+            answers = f.Run(new int[] { 2, 3, 5 }, 8);
+        }
+    }
+}
